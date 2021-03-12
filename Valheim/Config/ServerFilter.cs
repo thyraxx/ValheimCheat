5700113c7ad8ce150ee89dc06420311e35e20c98
Thyraxx <taz3008@msn.com>
2021-03-13 00:31:12 +0100

    Server filter model

---
+++ Valheim/Config/ServerFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valheim.Config
+{
+    public static class ServerFilter
+    {
+        public static bool Enabled { get; set; } = false;
+        public static bool HasPassword { get; set; } = true;
+
+        public static int Amount { get; set; } = 200;
+    }
+}
